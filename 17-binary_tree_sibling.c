a1208140dc083d8852e48abad210f3d67c6ba9ea
mr-mensah <mr-mensah@gmail.com>
2023-11-28 14:58:29 -0800

    mandatory

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - function that finds the sibling of a tree
+ * @node: node
+ * Return: sibling node
+*/
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
+}
